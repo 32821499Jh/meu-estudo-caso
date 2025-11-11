a64773b7a8d5447023d17944333f2f396d2bf08b
32821499Jh <145176448+32821499Jh@users.noreply.github.com>
2025-11-10 22:50:26 -0300

    mobile funcionando

---
+++ web/src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import { Box } from "@mui/material";
+import NavBar from "./components/NavBar";
+import AppRoutes from "./routes/AppRoutes";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      {/* Navbar fixa no topo */}
+      <NavBar />
+
+      {/* Conteúdo principal centralizado */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          minHeight: "calc(100vh - 64px)", // caso a altura da NavBar seja 64px
+          backgroundColor: "#f8f9fa",
+          padding: 2,
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: 800,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <AppRoutes />
+        </Box>
+      </Box>
+    </BrowserRouter>
+  );
+}
